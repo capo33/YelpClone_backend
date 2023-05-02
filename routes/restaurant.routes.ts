85869df08c9ae69a0b60fab08d9fbae8b4a0b168
Mohamed <s2000588@edu.bc.fi>
2023-05-02 20:51:20 +0300

    start the backend for YelpClone with postgrSQL and typescript

---
+++ routes/restaurant.routes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import * as restaurantController from '../controllers/restaurantController';
+
+const router = express.Router();
+
+router.get('/', restaurantController.getRestaurants);
+router.get('/:id', restaurantController.getRestaurant);
+router.post('/', restaurantController.createRestaurant);
+router.put('/:id', restaurantController.updateRestaurant);
+router.delete('/:id', restaurantController.deleteRestaurant);
+router.post('/:id/addReview', restaurantController.addReview);
+
+export default router;
